0b0f3d4bf02511bda4159c1e95a0a3de64fc8f43
Julian Zellner <julian@zellner.cc>
2022-12-21 14:39:22 +0100

    added basics in fetcher scripts

--- fetcher_scripts/console/bin/console.dart
+++ fetcher_scripts/console/bin/console.dart
@@ -1,5 +1,4 @@
-import 'package:console/console.dart' as console;
 
 void main(List<String> arguments) {
-  print('Hello world: ${console.calculate()}!');
+  print('Hello world !');
 }
---
+++ fetcher_scripts/console/lib/hello_fresh_fetcher.dart
@@ -0,0 +1,147 @@
+import 'dart:core';
+
+import 'package:console/hello_fresh_model.dart';
+import 'package:dio/dio.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'hello_fresh_fetcher.g.dart';
+
+@RestApi(baseUrl: '')
+abstract class RestClient {
+  factory RestClient(final Dio dio, {final String baseUrl}) = _RestClient;
+
+  @GET('/recipes/search')
+  Future<HelloFreshModelRecipeApiRecipeResponse> getRecipes({
+    @Query('country') required final String country,
+    @Query('ingredient') final List<String>? ingredients,
+    @Query('cuisine') final String? cuisine,
+    @Query('tag') final String? tags,
+    @Query('q') final String? query,
+    @Query('take') final int? take,
+    @Query('skip') final int? skip,
+  });
+
+  @GET('/recipes/{id}')
+  Future<HelloFreshModelRecipe> getSingleRecipe({
+    @Path('id') required final String id,
+  });
+
+  @GET('/tags')
+  Future<HelloFreshModelRecipeApiTagResponse> getTags({
+    @Query('country') required final String country,
+    @Query('take') final int? take,
+  });
+
+  @GET('/cuisines')
+  Future<HelloFreshModelRecipeApiCuisineResponse> getCuisines({
+    @Query('country') required final String country,
+    @Query('take') final int? take,
+  });
+
+  @GET('/ingredients')
+  Future<HelloFreshModelRecipeApiIngredientsResponse> getIngredients({
+    @Query('country') required final String country,
+    @Query('take') final int? take,
+    @Query('skip') final int? skip,
+  });
+}
+
+class HelloFreshFetcher {
+  static const String apiBaseUrl1 = 'https://www.hellofresh.de/gw/api';
+  static const Map<String, String> headers = <String, String>{
+    'Authorization':
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzE4MzgwNDIsImlhdCI6MTY2OTIwODI5OSwiaXNzIjoic2VuZiIsImp0aSI6ImM0ZDRmNDU5LTIyMzEtNGIzMi1iOTkxLTkzZjA3NWQ1ZmE2ZSJ9.XZh7xVvZ6HRyNkUYcXIyiD14msT9WR2dZ57kCcMa0ZE',
+  };
+  final RestClient client = RestClient(
+    Dio(BaseOptions(headers: headers)),
+    baseUrl: apiBaseUrl1,
+  );
+
+  TaskEither<Exception, HelloFreshModelRecipeApiRecipeResponse> fetchRecipes({
+    required final String country,
+    required final int take,
+    required final int skip,
+    final Option<List<String>> tags = const None<List<String>>(),
+    final Option<String> cuisine = const None<String>(),
+    final Option<List<String>> ingredients = const None<List<String>>(),
+    final Option<String> searchTerm = const None<String>(),
+  }) =>
+      TaskEither<Exception, HelloFreshModelRecipeApiRecipeResponse>.tryCatch(
+        () async => await client.getRecipes(
+          country: country,
+          take: take,
+          skip: skip,
+          tags: _combineFiltersForApi(filters: tags).toNullable(),
+          cuisine: cuisine.toNullable(),
+          ingredients: ingredients.toNullable(),
+        ),
+        (final Object error, final StackTrace stacktrace) => Exception(
+          'Failed to fetch recipes: $error, $stacktrace',
+        ),
+      );
+
+  TaskEither<Exception, HelloFreshModelRecipeApiTagResponse> fetchAllTags({
+    required final String country,
+    final Option<int> take = const None<int>(),
+  }) =>
+      TaskEither<Exception, HelloFreshModelRecipeApiTagResponse>.tryCatch(
+        () async => await client.getTags(
+          country: country,
+          take: take.toNullable(),
+        ),
+        (final Object error, final StackTrace stacktrace) => Exception(
+          'Failed to fetch tags: $error, $stacktrace',
+        ),
+      );
+
+  TaskEither<Exception, HelloFreshModelRecipe> fetchSingleRecipe({
+    required final String recipeId,
+  }) =>
+      TaskEither<Exception, HelloFreshModelRecipe>.tryCatch(
+        () async => await client.getSingleRecipe(id: recipeId),
+        (final Object error, final _) => Exception(
+          'Failed to fetch recipe with id: $recipeId: $error',
+        ),
+      );
+
+  TaskEither<Exception,
+      HelloFreshModelRecipeApiCuisineResponse> fetchAllCuisines({
+    required final String country,
+    final Option<int> take = const None<int>(),
+  }) =>
+      TaskEither<Exception, HelloFreshModelRecipeApiCuisineResponse>.tryCatch(
+        () async => await client.getCuisines(
+          country: country,
+          take: take.toNullable(),
+        ),
+        (final Object error, final _) => Exception(
+          'Failed to fetch cuisines: $error',
+        ),
+      );
+
+  TaskEither<Exception, HelloFreshModelRecipeApiIngredientsResponse>
+      fetchAllIngredientFamilies({
+    required final String country,
+    final Option<int> take = const None<int>(),
+    final Option<int> skip = const None<int>(),
+  }) =>
+          TaskEither<Exception,
+              HelloFreshModelRecipeApiIngredientsResponse>.tryCatch(
+            () async => await client.getIngredients(
+              country: country,
+              take: take.toNullable(),
+              skip: skip.toNullable(),
+            ),
+            (final Object error, final StackTrace stacktrace) => Exception(
+              'Failed to fetch ingredients: $error, $stacktrace',
+            ),
+          );
+}
+
+Option<String> _combineFiltersForApi({
+  required final Option<List<String>> filters,
+}) =>
+    filters.map(
+      (final List<String> allFilters) => allFilters.join(','),
+    );
