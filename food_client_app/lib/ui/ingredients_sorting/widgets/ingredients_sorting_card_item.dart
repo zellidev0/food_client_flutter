e54d37a40409e4ae7f74f40d889337d5350814e4
Julian <julian@zellner.cc>
2023-11-05 18:08:59 +0100

    fmt: format

--- food_client_app/lib/ui/ingredients_sorting/widgets/ingredients_sorting_card_item.dart
+++ food_client_app/lib/ui/ingredients_sorting/widgets/ingredients_sorting_card_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_client/ui/ingredients_sorting/ingredients_sorting_model.dart';
 import 'package:food_client/ui/ingredients_sorting/ingredients_sorting_view.dart';
 import 'package:fpdart/fpdart.dart';
