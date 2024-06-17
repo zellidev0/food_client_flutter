d7c2fa024c7643f6bdeafe5e94db2d6f1441b44b
Julian <julian@zellner.cc>
2024-06-17 08:39:37 +0200

    ref: simplify recipe view header and list widgets

--- food_client_app/lib/ui/home/widgets/recipe_header.dart
+++ food_client_app/lib/ui/home/widgets/recipe_header.dart
@@ -3,18 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:food_client/generated/locale_keys.g.dart';
 import 'package:food_client/ui/home/home_controller.dart';
 import 'package:food_client/ui/home/home_model.dart';
-import 'package:food_client/ui/home/widgets/filter_dialog.dart';
 import 'package:food_client/ui/home/widgets/single_filter_chip.dart';
 
 class RecipeHeader extends StatelessWidget {
   const RecipeHeader({
     super.key,
     required this.controller,
-    required this.filters,
   });
 
   final HomeController controller;
-  final List<HomeModelFilter> filters;
 
   @override
   Widget build(BuildContext context) => Row(
@@ -25,42 +22,15 @@ class RecipeHeader extends StatelessWidget {
             onPressed: controller.goToHistoryView,
           ),
           Expanded(child: Container()),
-          if (filters.isNotEmpty)
-            SingleFilterChip(
-              label: LocaleKeys.ui_home_view_filters_tags.tr(),
-              filters: filters
-                  .where(
-                    (final HomeModelFilter filter) =>
-                        filter is HomeModelFilterTag && filter.isSelected,
-                  )
-                  .toList(),
-              controller: controller,
-              onTap: () => controller.openDialog(
-                child: FilterDialog(
-                  onTap: controller.setFiltersSelected,
-                  allFilters: filters.whereType<HomeModelFilterTag>().toList(),
-                ),
-              ),
-            ),
+          SingleFilterChip<HomeModelFilterTag>(
+            label: LocaleKeys.ui_home_view_filters_tags.tr(),
+            controller: controller,
+          ),
           const SizedBox(width: 8),
-          if (filters.isNotEmpty)
-            SingleFilterChip(
-              label: LocaleKeys.ui_home_view_filters_cuisines.tr(),
-              controller: controller,
-              filters: filters
-                  .where(
-                    (final HomeModelFilter filter) =>
-                        filter is HomeModelFilterCuisine && filter.isSelected,
-                  )
-                  .toList(),
-              onTap: () => controller.openDialog(
-                child: FilterDialog(
-                  onTap: controller.setFiltersSelected,
-                  allFilters:
-                      filters.whereType<HomeModelFilterCuisine>().toList(),
-                ),
-              ),
-            ),
+          SingleFilterChip<HomeModelFilterCuisine>(
+            label: LocaleKeys.ui_home_view_filters_cuisines.tr(),
+            controller: controller,
+          ),
         ],
       );
 }
--- food_client_app/lib/ui/home/widgets/single_filter_chip.dart
+++ food_client_app/lib/ui/home/widgets/single_filter_chip.dart
@@ -1,36 +1,52 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_client/commons/view_state.dart';
 import 'package:food_client/generated/locale_keys.g.dart';
 import 'package:food_client/ui/home/home_controller.dart';
 import 'package:food_client/ui/home/home_model.dart';
+import 'package:food_client/ui/home/home_providers.dart';
+import 'package:food_client/ui/home/widgets/filter_dialog.dart';
 
-class SingleFilterChip extends StatelessWidget {
+class SingleFilterChip<T extends HomeModelFilter> extends ConsumerWidget {
   const SingleFilterChip({
     super.key,
     required this.label,
-    required this.filters,
     required this.controller,
-    required this.onTap,
   });
 
   final String label;
-  final List<HomeModelFilter> filters;
   final HomeController controller;
-  final VoidCallback onTap;
 
   @override
-  Widget build(BuildContext context) => FilterChip(
-        label: Text(
-          filters.isEmpty
-              ? label
-              : LocaleKeys.ui_home_view_filters_name_with_amount.tr(
-                  namedArgs: <String, String>{
-                    'name': label,
-                    'amount': filters.length.toString(),
-                  },
+  Widget build(BuildContext context, WidgetRef ref) =>
+      ref.watch(homeModelFiltersProvider).map(
+            data: (ViewStateData<List<HomeModelFilter>> value) {
+              final List<T> filters = value.data
+                  .whereType<T>()
+                  .where((final HomeModelFilter filter) => filter.isSelected)
+                  .toList();
+              return FilterChip(
+                label: Text(
+                  filters.isEmpty
+                      ? label
+                      : LocaleKeys.ui_home_view_filters_name_with_amount.tr(
+                          namedArgs: <String, String>{
+                            'name': label,
+                            'amount': filters.length.toString(),
+                          },
+                        ),
                 ),
-        ),
-        selected: filters.isNotEmpty,
-        onSelected: (_) => onTap(),
-      );
+                selected: filters.isNotEmpty,
+                onSelected: (_) => controller.openDialog(
+                  child: FilterDialog(
+                    onTap: controller.setFiltersSelected,
+                    allFilters: value.data.whereType<T>().toList(),
+                  ),
+                ),
+              );
+            },
+            error: (_) => const SizedBox.shrink(),
+            loading: (_) => const SizedBox.shrink(),
+          );
 }
